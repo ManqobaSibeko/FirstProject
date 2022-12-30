2b0706a2321df76ea16696b004dd6c46c73f5341
Manqoba Sibeko <manqsibeko@gmail.com>
2022-12-30 15:19:20 +0200

    My first commit

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("is not an alien");
+        }
+    }
+}
