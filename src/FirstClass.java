0680e6a70aa0a783d8a355e84196179b0d3c536e
Manqoba Sibeko <manqsibeko@gmail.com>
2022-12-30 15:32:11 +0200

    My second commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,7 +2,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
         boolean isAlien = false;
-        if (isAlien == false){
+        if (5>1){
+            //put one or more statement if the statement is true
             System.out.println("is not an alien");
         }
     }
