c39244b4a78734bff79b3b237999f4fa48ada0da
Manqoba Sibeko <manqsibeko@gmail.com>
2022-12-30 15:42:37 +0200

    My third commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,10 +1,17 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        int newValue =  67;
+
         boolean isAlien = false;
-        if (5>1){
+        if (isAlien == false){
             //put one or more statement if the statement is true
             System.out.println("is not an alien");
+        }
+
+        if(newValue  == 67){
+            System.out.println("perfect");
         }
     }
 }
